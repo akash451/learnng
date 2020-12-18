aeb8436d0ba49222c256d832bda7bbc75fbbd856
unknown <akash.a@kennect.in>
2020-12-19 01:02:49 +0530

    data binding and events

--- app.js
+++ app.js
@@ -2,11 +2,25 @@ new Vue({
     el:'#vue-app',
     data: {
          name: 'Akash',
-         job: 'Software Engineer'
+         job: 'Software Engineer',
+         website: 'https://www.google.com/',
+         age: 25,
+         X:0,
+         Y:0,
     },
     methods: {
+        add: function(inc){
+            this.age= this.age+ inc;
+        },
+        sub: function(dec){
+            this.age= this.age-dec;
+        },
         greet: function(time){
             return 'Good' +time+' '+ this.name;
+        },
+        updateXY: function(event){
+            this.X= event.offsetX;
+            this.Y= event.offsetY;
         }
     }
 })
